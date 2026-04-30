b2cc20669011a39709b0f547b74809f9860726ae
Karma-234 <busari1kamal@gmail.com>
2026-04-30 22:10:21 +0100

    ypdaet: metrics

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -11,16 +11,24 @@ var (
 	TransactionsTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "iso8583_transactions_total",
-			Help: "Total number of ISO 8583 transactions",
+			Help: "Total number of processed ISO 8583 transactions",
 		},
-		[]string{"mti", "response_code"},
+		[]string{"mti", "response_code", "merchant_id"},
 	)
 
-	TransactionDuration = prometheus.NewHistogram(
+	TransactionDuration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "iso8583_transaction_duration_seconds",
-			Help:    "Transaction processing duration",
+			Help:    "Duration of ISO 8583 transaction processing",
 			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"mti"},
+	)
+
+	ActiveConnections = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "iso8583_active_connections",
+			Help: "Current number of active ISO 8583 connections",
 		},
 	)
 )
@@ -28,8 +36,10 @@ var (
 func init() {
 	prometheus.MustRegister(TransactionsTotal)
 	prometheus.MustRegister(TransactionDuration)
+	prometheus.MustRegister(ActiveConnections)
 }
 
+// Handler returns Prometheus metrics handler for /metrics endpoint
 func Handler() http.Handler {
 	return promhttp.Handler()
 }
