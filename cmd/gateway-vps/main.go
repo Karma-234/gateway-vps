be63f983b701539649f53d0607af9d32a04c5a6d
Karma-234 <busari1kamal@gmail.com>
2026-04-30 21:41:50 +0100

    add: tls config

--- cmd/gateway-vps/main.go
+++ cmd/gateway-vps/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,15 +15,33 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
-	server, err := iso.NewServer(":8080")
-	if err != nil {
-		log.Fatalf("failed to create server: %v", err)
-	}
-	log.Println("ISO 8583 Gateway listening on :8080 (Simple TCP)")
+	go func() {
+
+		srv, err := iso.NewServer(":8080")
+		defer srv.Close()
+		if err != nil {
+			log.Fatalf("failed to create server: %v", err)
+		}
+		// if err := srv.Start(); err != nil {
+		// 	log.Fatal(err)
+		// }
+		log.Println("ISO 8583 Gateway listening on :8080 (Simple TCP)")
+	}()
 
 	http.HandleFunc("/health", health.Handler)
 
+	server := &http.Server{Addr: ":8081", TLSConfig: &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}}
+
 	log.Println("Health check at: http://localhost:8081/health")
+	go func() {
+		log.Println("HTTP server listening on :8081")
+		if err := server.ListenAndServeTLS("/certs/server.crt", "/certs/server.key"); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("HTTP server error: %v", err)
+		}
+	}()
+
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
 	<-done
